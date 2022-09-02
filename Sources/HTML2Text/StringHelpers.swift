a657d1c7a0c800e76fbd506b89011a9513071a60
Brett Terpstra <me@brettterpstra.com>
2022-09-02 11:03:28 -0500

    Fixing some warnings

--- Sources/HTML2Text/StringHelpers.swift
+++ Sources/HTML2Text/StringHelpers.swift
@@ -49,12 +49,12 @@ extension String {
     ///
     /// - returns: The equivalent Range<String.Index>.
     func range(from nsRange: NSRange) -> Range<String.Index>? {
-        guard
-            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
-            let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
-            let from = String.Index(from16, within: self),
-            let to = String.Index(to16, within: self)
-            else { return nil }
+//        guard
+//            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+//            let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
+//            let from = String.Index(from16, within: self),
+//            let to = String.Index(to16, within: self)
+//            else { return nil }
         // return from ..< to
         return Range<String.Index>(nsRange, in: self)
     }
